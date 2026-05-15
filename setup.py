599abab25d0a13ec6b47e37fa3b533c97518b304
Sadat <muhaiminulsadat@gmail.com>
2026-05-15 12:31:31 +0600

    setup mongodb and store data with notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Sadat",
+    author_email="muhaiminulsadat@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
