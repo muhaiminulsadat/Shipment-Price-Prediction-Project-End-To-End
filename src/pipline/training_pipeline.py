5ea2675ac1baadf65a80a77aea0cbd28f6a82b13
Sadat <muhaiminulsadat@gmail.com>
2026-06-02 08:34:31 +0600

    add data validation step in training pipeline

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -5,8 +5,7 @@ from src.configuration.mongo_operations import MongoDBOperation
 from src.entity.artifact_entity import DataIngestionArtifacts, DataValidationArtifacts
 from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from src.components.data_ingestion import DataIngestion
-
-# from src.components.data_validation import DataValidation
+from src.components.data_validation import DataValidation
 
 
 class TrainPipeline:
@@ -34,33 +33,34 @@ class TrainPipeline:
             raise shippingException(e, sys) from e
 
     # This method is used to start the data validation
-    # def start_data_validation(
-    #     self, data_ingestion_artifact: DataIngestionArtifacts
-    # ) -> DataValidationArtifacts:
-    #     logging.info("Entered the start_data_validation method of TrainPipeline class")
-    #     try:
-    #         data_validation = DataValidation(
-    #             data_ingestion_artifacts=data_ingestion_artifact,
-    #             data_validation_config=self.data_validation_config,
-    #         )
-    #         data_validation_artifact = data_validation.initiate_data_validation()
-    #         logging.info("Performed the data validation operation")
-    #         logging.info(
-    #             "Exited the start_data_validation method of TrainPipeline class"
-    #         )
-    #         return data_validation_artifact
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifacts
+    ) -> DataValidationArtifacts:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifacts=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation operation")
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+            return data_validation_artifact
 
-    #     except Exception as e:
-    #         raise shippingException(e, sys) from e
+        except Exception as e:
+            raise shippingException(e, sys) from e
 
     # This method is used to start the training pipeline
     def run_pipeline(self) -> None:
         logging.info("Entered the run_pipeline method of TrainPipeline class")
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(
-            #     data_ingestion_artifact=data_ingestion_artifact
-            # )
+            
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
             logging.info("Exited the run_pipeline method of TrainPipeline class")
 
